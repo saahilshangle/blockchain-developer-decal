f4c4603ed78d29613ef5eedecb62ce74b4e0ce06
Saahil Shangle <saahilshangle@gmail.com>
2020-03-16 22:02:55 -0700

    Finished week4 hw

--- hw4-Interact with Ethereum Through Web3/web3.js
+++ hw4-Interact with Ethereum Through Web3/web3.js
@@ -1,12 +1,98 @@
 // Commands for you to execute in interactive node window
 // If you want, you can also deploy it in a simple web app
 const Web3 = require('web3')
+//import Web3 from 'web3';
 // Get the endpoint under the correct test net in Infura
-var web3 = new Web3(new Web3.providers.HttpProvider("{Your Infura Endpoint}"));
+var web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/45b065b192874651b3ff5dfad4c2cf6e"));
 // Get your contract's deployed address, access that through the link in Remix console.
-const address = "Your Contract Address";
+const address = "0xE0c86A4f029Dac6C6986448aFb62B135c9279dA0";
 // Your contract's ABI, from Remix IDE
-const ABI = "{Your contract's ABI info}";
+const ABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "withdrawAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [
+			{
+				"name": "remainingBal",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "balance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "accountAddress",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "LogDepositMade",
+		"type": "event"
+	}
+];
 
 // Get properties regarding the function calls
 web3.eth.getBalance
